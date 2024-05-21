a5df866fcab50447fa5b68d85b0acf351ee74f3e
mekeyace <mekeyace@nate.com>
2024-05-21 16:21:17 +0900

    main

---
+++ game/game1.java
@@ -0,0 +1,12 @@
+package game;
+
+public class game1 {
+
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("유재석");
+		sb.append("강호동");
+		System.out.println(sb);
+	}
+
+}
---
+++ game/game2.java
@@ -0,0 +1,12 @@
+package game;
+
+public class game2 {
+	public static void main(String[] args) {
+		new game2_box();
+	}
+}
+class game2_box{
+	public game2_box() {
+		
+	}
+}
